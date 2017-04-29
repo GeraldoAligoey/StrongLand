59e079a757a5162bf914cdf3a742a0f1413a89bd
eljaelee96 <eljaelee96@gmail.com>
2017-04-30 04:31:19 +0800

    twitter failedto get app auth token

--- app/src/main/java/me/eljae/strongland/News.java
+++ app/src/main/java/me/eljae/strongland/News.java
@@ -2,6 +2,7 @@ package me.eljae.strongland;
 
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.support.v4.view.ViewGroupCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,25 +11,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.OAuthSigning;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.tweetui.SearchTimeline;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 
-public class News extends Fragment {
+public class News extends ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.activity_news, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.activity_news, container, false);;
         final SearchTimeline searchTimeline = new SearchTimeline.Builder()
                 .query("#landslide")
                 .build();
         final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(getContext())
                 .setTimeline(searchTimeline).build();
-        ListView listView = (ListView)view.findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+        setListAdapter(adapter);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+
     }
 }
