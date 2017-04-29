70aa341c8c8382b5987e731c794dbb4255d21707
hrmun <hrmun.anthony@gmail.com>
2017-04-29 22:03:37 +0800

    Add map activity and fragment

--- app/src/main/java/me/eljae/strongland/MainActivity.java
+++ app/src/main/java/me/eljae/strongland/MainActivity.java
@@ -1,43 +1,65 @@
 package me.eljae.strongland;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TabHost;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.*;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabHost tabHost = (TabHost)findViewById(R.id.tabHost);
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
         tabHost.setup();
 
+        TabHost.TabSpec spec;
+        Intent intent;
+
         // Main tab
-        TabHost.TabSpec spec = tabHost.newTabSpec("Main");
-        spec.setContent(R.id.mainTab);
-        spec.setIndicator("Main");
+        spec = tabHost.newTabSpec("Map");
+        spec.setIndicator("Map");
+        intent = new Intent(this, Map.class);
+        spec.setContent(intent);
         tabHost.addTab(spec);
+
 
         // Information tab
         spec = tabHost.newTabSpec("Information");
-        spec.setContent(R.id.infoTab);
         spec.setIndicator("Information");
-        tabHost.addTab(spec);
+        intent = new Intent(this, AddNewRecord.class);
+        spec.setContent(intent);
+     //   tabHost.addTab(spec);
 
         // News tab
-        spec = tabHost.newTabSpec("News");
-        spec.setContent(R.id.newsTab);
+        spec = tabHost.newTabSpec("news");
         spec.setIndicator("News");
-        tabHost.addTab(spec);
+        intent = new Intent(this, News.class);
+        spec.setContent(intent);
+     //   tabHost.addTab(spec);
 
         // Forecast tab
         spec = tabHost.newTabSpec("Forecast");
-        spec.setContent(R.id.forecastTab);
         spec.setIndicator("Forecast");
-        tabHost.addTab(spec);
+        intent = new Intent(this, Forecast.class);
+        spec.setContent(intent);
+      //  tabHost.addTab(spec);
+
+        tabHost.setCurrentTab(1);
+
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener()
+        {
+            @Override
+            public void onTabChanged(String tabId)
+            {
+                // display the name of the tab whenever a tab is changed
+                Toast.makeText(getApplicationContext(), tabId, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
