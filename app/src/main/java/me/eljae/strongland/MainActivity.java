7b414984a9dc02ff63ed35a6c4db405347e8a033
hrmun <hrmun.anthony@gmail.com>
2017-04-29 15:00:49 +0800

    Add basic tab host

--- app/src/main/java/me/eljae/strongland/MainActivity.java
+++ app/src/main/java/me/eljae/strongland/MainActivity.java
@@ -2,12 +2,42 @@ package me.eljae.strongland;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = (TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        // Main tab
+        TabHost.TabSpec spec = tabHost.newTabSpec("Main");
+        spec.setContent(R.id.mainTab);
+        spec.setIndicator("Main");
+        tabHost.addTab(spec);
+
+        // Information tab
+        spec = tabHost.newTabSpec("Information");
+        spec.setContent(R.id.infoTab);
+        spec.setIndicator("Information");
+        tabHost.addTab(spec);
+
+        // News tab
+        spec = tabHost.newTabSpec("News");
+        spec.setContent(R.id.newsTab);
+        spec.setIndicator("News");
+        tabHost.addTab(spec);
+
+        // Forecast tab
+        spec = tabHost.newTabSpec("Forecast");
+        spec.setContent(R.id.forecastTab);
+        spec.setIndicator("Forecast");
+        tabHost.addTab(spec);
     }
 }
